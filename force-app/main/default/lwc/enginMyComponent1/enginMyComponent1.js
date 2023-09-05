0858b55e9d3a1e3c334bf282b607c80737fd29ba
senolyildiz <senolyildiz9801@gmail.com>
2023-09-05 17:15:16 +0100

    aa

--- force-app/main/default/lwc/enginMyComponent1/enginMyComponent1.js
+++ force-app/main/default/lwc/enginMyComponent1/enginMyComponent1.js
@@ -1,3 +1,11 @@
 import { LightningElement } from 'lwc';
 
-export default class EnginMyComponent1 extends LightningElement {}
+export default class EnginMyComponent1 extends LightningElement {
+
+
+name;
+namehandler(event){
+    this.name=event.target.value;
+}
+
+}
