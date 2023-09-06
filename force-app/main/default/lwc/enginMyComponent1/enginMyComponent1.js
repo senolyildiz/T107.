81635146a16d683aefe29342a1c4d0d89f6d8a31
senolyildiz <senolyildiz9801@gmail.com>
2023-09-06 09:09:45 +0100

    a

--- force-app/main/default/lwc/enginMyComponent1/enginMyComponent1.js
+++ force-app/main/default/lwc/enginMyComponent1/enginMyComponent1.js
@@ -2,10 +2,19 @@ import { LightningElement } from 'lwc';
 
 export default class EnginMyComponent1 extends LightningElement {
 
+firstName;
+lastName;
+firstNameHandler(event){
+    this.firstName=event.target.value;
+}
+lastNameHandler(event){
+    this.lastName=event.target.value;
+}
 
-name;
-namehandler(event){
-    this.name=event.target.value;
+
+get fullName(){
+
+    return `${this.firstName} ${this.lastName}`;
 }
 
 }
