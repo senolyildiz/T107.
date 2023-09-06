a69a8775009373b656e7410c70aa008143a3fd3a
senolyildiz <senolyildiz9801@gmail.com>
2023-09-06 22:28:51 +0100

    q

---
+++ force-app/main/default/lwc/createAccount/createAccount.js
@@ -0,0 +1,6 @@
+import { LightningElement } from 'lwc';
+
+export default class CreateAccount extends LightningElement {
+
+    
+}
---
+++ force-app/main/default/lwc/iteratorExample/iteratorExample.js
@@ -0,0 +1,13 @@
+import { LightningElement } from 'lwc';
+
+export default class IteratorExample extends LightningElement {
+
+    data= [
+        {Id:1, Name:'Ali Yildiz', Score:123456},
+        {Id:2, Name:'Veli Yildiz', Score:125456},
+        {Id:3, Name:'Mehmet Yildiz', Score:333456},
+        {Id:4, Name:'Yusuf Yildiz', Score:123556},
+        {Id:5, Name:'Hasan Yildiz', Score:127456}
+    ]
+
+}
