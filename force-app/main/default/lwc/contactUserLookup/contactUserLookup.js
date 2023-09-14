0748b97802f52402d97377b1f80faae3aff733ce
senolyildiz <senolyildiz9801@gmail.com>
2023-09-14 21:05:26 +0100

    a

--- force-app/main/default/lwc/contactUserLookup/contactUserLookup.js
+++ force-app/main/default/lwc/contactUserLookup/contactUserLookup.js
@@ -3,14 +3,26 @@ import getUserInfoByEmail from '@salesforce/apex/ContactUserController.getUserIn
 
 export default class ContactUserLookup extends LightningElement {
     @api recordId;
-    userInfo;
+    @api userFound=false;
+    @api userNotFound=false;
+    @api contactHasNoEmail=false;
+    @api userInfo;
+
+    
 
     @wire(getUserInfoByEmail, { contactId: '$recordId' })
+   
     wiredUserInfo({ error, data }) {
         if (data) {
+            this.userFound=true;
             this.userInfo = data;
         } else if (error) {
             console.error(error);
+        } else {
+            // User not found or contact doesn't have an email
+            const contactWithoutEmail= this.recordId && !this.userInfo.email;
+            this.contactHasNoEmail= contactWithoutEmail;
+            this.userNotFound= !contactWithoutEmail;
         }
     }
 }
